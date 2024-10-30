0fea79b6128ede84c504915e7bb576928f963f33
Oualid Safir <oualid.safir@cipalschaubroeck.be>
2024-10-30 13:58:28 +0100

    adjustments to the top-gradient bar, made it responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
-import logo from '../assets/Laassaara Foundation (2).svg';
-import anbi from '../assets/logo-anbi (1).svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPiggyBank, faCreditCard  } from '@fortawesome/free-solid-svg-icons';
-
+import React, { useState } from "react";
+import logo from "../assets/Laassaara Foundation (2).svg";
+import anbi from "../assets/logo-anbi (1).svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faPiggyBank,
+  faCreditCard,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,30 +16,39 @@ const Navbar = () => {
   };
 
   const menuLinks = [
-    { href: '/', label: 'Home' },
-    { href: '/over', label: 'Over ons' },
-    { href: '/projecten', label: 'Projecten' },
-    { href: '/doneer', label: 'Doneer' },
-    { href: '/contact', label: 'Contact' },
+    { href: "/", label: "Home" },
+    { href: "/over", label: "Over ons" },
+    { href: "/projecten", label: "Projecten" },
+    { href: "/doneer", label: "Doneer" },
+    { href: "/contact", label: "Contact" },
   ];
 
   return (
-    <nav >
-     <div className="relative bg-gradient-to-r from-green-400 to-blue-400 py-3 px-6 flex justify-between items-center text-white shadow-md">
-  <div className="flex items-center space-x-2">
-    <FontAwesomeIcon icon={faEnvelope} className="text-white text-base" />
-    <p className="text-sm font-medium tracking-wide">info@laassaarafoundation.nl</p>
-  </div>
-  
-  <div className="flex items-center mr-24 space-x-2">
-    <FontAwesomeIcon icon={faCreditCard} className="text-white text-lg " />
-    <p className="tracking-wider">
-      <span className="font-bold">NL67 TRIO 0320 5916 89</span>
-    </p>
-  </div>
-  
-  <img src={anbi} alt="ANBI" className="h-10 w-10 opacity-90 hover:opacity-100 transition-opacity duration-200" />
-</div>
+    <nav>
+      <div className="relative bg-gradient-to-r from-green-400 to-blue-400 py-3 px-6 flex flex-col md:flex-row space-y-4  md:space-y-0 md:justify-between  items-center text-white shadow-md">
+        <div className="flex items-center space-x-2">
+          <FontAwesomeIcon icon={faEnvelope} className="text-white text-base" />
+          <p className="text-sm font-medium tracking-wide">
+            info@laassaarafoundation.nl
+          </p>
+        </div>
+
+        <div className="flex items-center md:mr-24 space-x-2">
+          <FontAwesomeIcon
+            icon={faCreditCard}
+            className="text-white text-lg "
+          />
+          <p className="tracking-wider">
+            <span className="font-bold">NL67 TRIO 0320 5916 89</span>
+          </p>
+        </div>
+
+        <img
+          src={anbi}
+          alt="ANBI"
+          className="h-10 w-10 opacity-90 hover:opacity-100 transition-opacity duration-200"
+        />
+      </div>
 
       <div className="container mx-auto md:mt-2  flex justify-between items-center">
         {/* Logo */}
@@ -46,7 +58,6 @@ const Navbar = () => {
             alt="Logo"
             className="h-[119px] w-[119px]"
           />
-          
         </div>
 
         {/* Hamburger Menu Icon for Mobile */}
@@ -90,7 +101,11 @@ const Navbar = () => {
       </div>
 
       {/* Mobile Menu */}
-      <div className={`md:hidden bg-opacity-95 absolute h-[100%] pb-5 z-50 top-26  w-full bg-white transition-all items-center duration-300 ease-in-out ${isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}`}>
+      <div
+        className={`md:hidden bg-opacity-95 absolute h-[100%] pb-5 z-50 top-26  w-full bg-white transition-all items-center duration-300 ease-in-out ${
+          isOpen ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+      >
         <div className="flex flex-col   justify-center items-center w-full space-y-8 mt-4">
           {menuLinks.map((link) => (
             <a
