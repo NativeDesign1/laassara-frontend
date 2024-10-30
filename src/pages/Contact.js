ba93ee4ae955b98bd872603b548dffcb043c01fc
Oualid Safir <oualid.safir@cipalschaubroeck.be>
2024-10-30 13:17:55 +0100

    adjusted the contact form , made some updates

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -54,7 +54,7 @@ const Contact = () => {
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md relative">
           <h2 className=" mb-4 text-4xl tracking-tight font-bold text-center text-gray-700 dark:text-white">Contacteer Ons</h2>
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-            Heb je vragen? Wil je graag feedback sture? Interesse in een samenwerking? Laat ons weten.
+            Heb je vragen? Wil je graag feedback sturen? Interesse in een samenwerking? Laat het ons weten.
           </p>
           <form onSubmit={handleSubmit} className="space-y-8">
             <div>
@@ -65,7 +65,7 @@ const Contact = () => {
                 value={formData.email}
                 onChange={handleChange}
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="name@flowbite.com"
+                placeholder="name@email.com"
                 required
               />
             </div>
@@ -77,19 +77,19 @@ const Contact = () => {
                 value={formData.subject}
                 onChange={handleChange}
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="Let us know how we can help you"
+                placeholder="Hoe kunnen we je helpen?"
                 required
               />
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Jou vraag</label>
+              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Jouw vraag</label>
               <textarea
                 id="message"
                 rows="6"
                 value={formData.message}
                 onChange={handleChange}
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Leave a comment..."
+                placeholder="Laat een bericht achter..."
                 required
               ></textarea>
             </div>
